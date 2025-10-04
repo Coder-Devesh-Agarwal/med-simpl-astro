4f9b9ab206b43ea5aed824bd344cd146005190e4
Geeklord <deveshagarwal9271@gmail.com>
2025-10-04 22:00:01 +0530

    feat: update site URL to include HTTPS for configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'static.medsimpl.com',
+  site: 'https://static.medsimpl.com',
   vite: {
     plugins: [tailwindcss()],
   },
