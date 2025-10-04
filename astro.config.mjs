7dde445331b39784e46bd74ed651cb2dcbe883ce
Geeklord <deveshagarwal9271@gmail.com>
2025-10-04 21:32:31 +0530

    feat: update site URL and base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://coder-devesh-agarwal.github.io',
+  base: '/med-simpl-astro',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
